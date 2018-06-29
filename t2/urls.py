2ac46f3acf762d324587ed32ae5174e08779c354
haosen <haosen@berkeley.edu>
2018-06-29 03:58:42 -0400

    T1&T2

--- t2/urls.py
+++ t2/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('p5/', views.p5, name='p5'),
+  path('p1/', views.p1, name='p1'),
+  path('p2/', views.p2, name='p2'),
+  path('p3/', views.p3, name='p3'),
+  path('p4/', views.p4, name='p4'),
     path('', views.index, name='index'),
 ]
--- t2/views.py
+++ t2/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 't2/index.html')
+def p5(request):
+    return render(request, 't2/p5.html')
+def p1(request):
+    return render(request, 't2/p1.html')
+def p2(request):
+    return render(request, 't2/p2.html')
+def p3(request):
+    return render(request, 't2/p3.html')
+def p4(request):
+    return render(request, 't2/p4.html')
